6cd933ec19ccc5e291ff389be23b9c24ea23d5e9
aereal <aereal@aereal.org>
2019-10-12 13:46:12 +0900

    test

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import { App } from "@aws-cdk/core";
 import { DBStack } from "./stacks/db";
 import { NetworkStack } from "./stacks/network";
 
-const app = new App();
+export const app = new App();
 const { defaultVPC } = new NetworkStack(app, "network", {});
 new DBStack(app, "db", { vpc: defaultVPC });
 app.synth();
