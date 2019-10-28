3667c6e4e314b5e475987d5417ad6c19c82388fc
aereal <aereal@aereal.org>
2019-10-29 00:05:54 +0900

    read from lib/VERSION

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,11 +1,13 @@
 import { region } from 'firebase-functions'
 import { readFileSync } from 'fs'
+import { join } from 'path';
 
 export const currentVersion = region('asia-northeast1').https.onRequest((_, res) => {
+  const versionFile = join(__dirname, 'VERSION');
   let error: any | undefined = undefined;
   let currentVersion: string | undefined = undefined;
   try {
-    const buf = readFileSync('VERSION')
+    const buf = readFileSync(versionFile)
     currentVersion = buf.toString();
   } catch (e) {
     error = e
