87e502e75ed488b9f292e1e1272973652d08a8a0
aereal <aereal@aereal.org>
2019-10-28 23:47:57 +0900

    specify region

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,7 +1,7 @@
-import functions from 'firebase-functions';
+import { region } from 'firebase-functions'
 import { readFileSync } from 'fs'
 
-export const currentVersion = functions.https.onRequest((_, res) => {
+export const currentVersion = region('asia-northeast1').https.onRequest((_, res) => {
   let error: any | undefined = undefined;
   let currentVersion: string | undefined = undefined;
   try {
